21a66b3b6a61672fd7865df1fe559fb56145fe4d
Wfuger <wfuger@gmail.com>
2016-11-06 13:14:34 -0600

    initial commit

---
+++ lib/cards.ex
@@ -0,0 +1,12 @@
+# defmodule creates a module called Cards
+# Module is a collection of methods or functions
+defmodule Cards do
+  # def creates a new function
+  # No return needed elixir has implicit return
+  def create_deck do
+    # Convential to use double quotes everywhere
+    # Square brackets create a list
+    ["Ace", "Two", "Three"]
+  end
+
+end
