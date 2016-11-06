af6e52c4ab6f4c6d79e51b1e1437891cdaea722c
Wfuger <wfuger@gmail.com>
2016-11-06 15:37:23 -0600

    save, load, create hand methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,7 +6,18 @@ defmodule Cards do
   def create_deck do
     # Convential to use double quotes everywhere
     # Square brackets create a list
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    """
+    List comprehension is a mapping function ie returns a new array
+    Runs multiple list comprehensions at same time and returns into 1 array
+    Nesting comprehensions return nested arrays
+    """
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+
   end
 
   def shuffle(deck) do
@@ -14,5 +25,36 @@ defmodule Cards do
     # Arity of one
     Enum.shuffle(deck)
   end
-  
+
+  def contains?(deck, card) do
+    # Methods can have question mark to denote a boolean return
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    # Returns a tuple {[hand],[rest of the deck]}
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    # Calling an Erlang method to save to file system
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    # case statements similar to switch
+    case File.read(filename) do
+      { :ok, binary } -> :erlang.binary_to_term binary
+      { :error, _reason } -> "That file does not exist"
+    end
+
+  end
+
+  def create_hand(hand_size) do
+
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
